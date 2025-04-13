4f46ec16d84515a7f6b7568958337f37792889c8
Matheus Rafael <matheusrafael77@hotmail.com>
2025-04-13 18:36:26 -0300

    feat change folder name

---
+++ big-chat-brasil-api/src/test/java/com/api/big_chat_brasil/BigChatBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.big_chat_brasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigChatBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
